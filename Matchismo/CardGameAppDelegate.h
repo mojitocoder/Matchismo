bf2c7ba781d5c55f0c4371d13d98ebea6063ee03
Quynh Nguyen <quynh@quynhs-imac.home>
2014-09-18 20:37:56 +0100

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Quynh Nguyen on 18/09/2014.
+//  Copyright (c) 2014 ___QuynhNguyen___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.m
@@ -0,0 +1,29 @@
+//
+//  CardGameViewController.m
+//  Matchismo
+//
+//  Created by Quynh Nguyen on 18/09/2014.
+//  Copyright (c) 2014 ___QuynhNguyen___. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface CardGameViewController ()
+
+@end
+
+@implementation CardGameViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
